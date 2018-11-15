9a4f21341511d1526667c79b13ba931575a02b90
Jarrod Thibodeau <thibodeaujz@gmail.com>
2018-11-15 18:45:06 -0500

    Updating to allow for query params

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const renderer = require('vue-server-renderer').createRenderer();
 const Vue = require('vue');
+const { parse } = require('url');
 
 module.exports = (req, res) => {
+    const { query } = parse(req.url, true);
+    const { character = 'not waluigi' } = query;
+
     const app = new Vue({
         data: {
-            url: req.url
+            character
         },
-        template: `<div> The URL here is... {{ url }}`
-    })
+        template: `<div> Your favorite super smash brother is.... {{ character }} </div>`
+    });
 
     renderer.renderToString(app, (err, html) => {
         if (err) {
-            res.status(500).end('shrug life')
+            res.status(500).end('Internal Server Error');
         }
 
         res.end(
+            `<!DOCTYPE html>
+            <html lang="en">
+                <head><title>Hello</title></head>
+                <body> ${html} </body>
+            </html>
             `
-                ${html}
-            `
-        )
+        );
     });
 }
